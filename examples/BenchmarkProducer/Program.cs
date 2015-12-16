bc49a6aa7826f6b6f88261dd4d22502762278d99
Andreas Heider <andreas@heider.io>
2015-12-16 01:05:45 +0000

    Multithreaded BenchmarkProducer

--- examples/BenchmarkProducer/Program.cs
+++ examples/BenchmarkProducer/Program.cs
@@ -1,25 +1,21 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using RdKafka;
 
 namespace BenchmarkProducer
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Produce(string topicName, long numMessages)
         {
-            var stopwatch = new Stopwatch();
-
-            long numMessages = 1000000;
-
             using (Producer producer = new Producer("127.0.0.1:9092"))
             {
-                Topic topic = producer.Topic(args[0]);
+                Topic topic = producer.Topic(topicName);
                 Console.WriteLine($"{producer.Name} producing on {topic.Name}");
-
-                stopwatch.Start();
-
                 for (int i = 0; i < numMessages; i++)
                 {
                     byte[] data = Encoding.UTF8.GetBytes(i.ToString());
@@ -30,9 +26,26 @@ namespace BenchmarkProducer
                 Console.WriteLine("Shutting down");
             }
 
+        }
+
+        public static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            long numMessages = 1000000;
+            int numThreads = 4;
+
+            stopwatch.Start();
+
+            var tasks = Enumerable.Range(0, numThreads)
+                .Select(x => Task.Factory.StartNew(() => Produce(args[0], numMessages),
+                    CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default))
+                .ToArray();
+            Task.WaitAll(tasks);
+
             stopwatch.Stop();
-            Console.WriteLine($"Sent {numMessages} messages in {stopwatch.Elapsed}");
-            Console.WriteLine($"{numMessages / stopwatch.Elapsed.TotalSeconds:F0} messages/second");
+            Console.WriteLine($"Sent {numMessages * numThreads} messages in {stopwatch.Elapsed}");
+            Console.WriteLine($"{numMessages * numThreads / stopwatch.Elapsed.TotalSeconds:F0} messages/second");
         }
     }
 }
