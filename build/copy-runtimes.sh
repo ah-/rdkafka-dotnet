0001d8cd07b55ef355fd7ae69adbee25eae5ea62
Andreas Heider <andreas@heider.io>
2015-12-18 22:26:24 +0000

    First shot at a continuous integration build

---
+++ build/copy-runtimes.sh
@@ -0,0 +1,28 @@
+set -e
+set -x
+
+if [ -z "$RDKAFKA_DOTNET_DIR" ]; then
+    RDKAFKA_DOTNET_DIR="$(dirname "$0")/.."
+fi
+
+mkdir -p "$RDKAFKA_DOTNET_DIR/.cache"
+pushd "$RDKAFKA_DOTNET_DIR/.cache"
+
+if [ ! -e nuget.exe ]; then
+    wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+fi
+
+mono nuget.exe install RdKafka.Internal.librdkafka-Darwin -Prerelease -Version 0.9.1-pre
+
+DYLIB=`pwd`/RdKafka.Internal.librdkafka-Darwin.0.9.1-pre/runtimes/osx/native/librdkafka.1.dylib
+
+popd
+
+RUNTIMES="osx.10.11-x64 osx.10.10-x64 osx.10.9-x64"
+
+for RUNTIME in $RUNTIMES
+do
+    TARGET="$RDKAFKA_DOTNET_DIR/src/RdKafka/runtimes/$RUNTIME/native"
+    mkdir -p $TARGET
+    cp $DYLIB $TARGET/librdkafka.dylib
+done
