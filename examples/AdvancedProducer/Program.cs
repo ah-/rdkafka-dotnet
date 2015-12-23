dc1f17df309e757a492a71929f3036e9c349196d
Andreas Heider <andreas@heider.io>
2015-12-24 00:11:53 +0100

    Add AdvancedConsumer example

---
+++ examples/AdvancedProducer/Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using RdKafka;
+
+namespace AdvancedProducer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (Producer producer = new Producer(args[0]))
+            {
+                var topicConfig = new TopicConfig
+                {
+                    CustomPartitioner = (top, key, cnt) =>
+                    {
+                        var kt = (key != null) ? Encoding.UTF8.GetString(key, 0, key.Length) : "(null)";
+                        int partition = (key?.Length ?? 0) % cnt;
+                        bool available = top.PartitionAvailable(partition);
+                        Console.WriteLine($"Partitioner topic: {top.Name} key: {kt} partition count: {cnt} -> {partition} {available}");
+                        return partition;
+                    }
+                };
+                Topic topic = producer.Topic(args[1], topicConfig);
+                Console.WriteLine($"{producer.Name} producing on {topic.Name}. q to exit.");
+
+                string text;
+                while ((text = Console.ReadLine()) != "q")
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(text);
+                    byte[] key = null;
+                    // Use the first word as the key
+                    int index = text.IndexOf(" ");
+                    if (index != -1) {
+                        key = Encoding.UTF8.GetBytes(text.Substring(0, index));
+                    }
+
+                    Task<DeliveryReport> deliveryReport = topic.Produce(data, key);
+                    var unused = deliveryReport.ContinueWith(task =>
+                    {
+                        Console.WriteLine($"Partition: {task.Result.Partition}, Offset: {task.Result.Offset}");
+                    });
+                }
+            }
+        }
+    }
+}
