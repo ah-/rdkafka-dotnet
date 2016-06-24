043d3c99e3a1b9ab3e65f30a0b5f7248c3c812c1
MaximGurschi <max.gurschi@gmail.com>
2016-06-24 11:52:38 +0100

    Make RdKafka.Offset fields const
    https://github.com/ah-/rdkafka-dotnet/issues/39

--- src/RdKafka/Offset.cs
+++ src/RdKafka/Offset.cs
@@ -6,21 +6,21 @@ namespace RdKafka
         /// <summary>
         /// Start consuming from beginning of kafka partition queue: oldest msg
         /// </summary>
-        public static long Beginning = -2;
+        public const long Beginning = -2;
 
         /// <summary>
         /// Start consuming from end of kafka partition queue: next msg
         /// </summary>
-        public static long End = -1;
+        public const long End = -1;
 
         /// <summary>
         /// Start consuming from offset retrieved from offset store
         /// </summary>
-        public static long Stored = -1000;
+        public const long Stored = -1000;
 
         /// <summary>
         /// Invalid offset
         /// </summary>
-        public static long Invalid = -1001;
+        public const long Invalid = -1001;
     }
 }
